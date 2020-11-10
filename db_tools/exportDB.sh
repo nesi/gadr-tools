b170ce5d0792af222b4e7a8716d11429ac89e8a5
eirianop <59072453+eirianop@users.noreply.github.com>
2020-11-10 15:23:25 +1300

    Update exportDB.sh

--- db_tools/exportDB.sh
+++ db_tools/exportDB.sh
@@ -3,6 +3,6 @@
 POSTGRES=$(docker ps -aqf "name=compose-services_postgres_1")
 
 docker exec -it $POSTGRES pg_dump -C -h localhost -U fence_user fence_db > fence_dump.db
-docker exec -it $POSTGRES pg_dump -C -h localhost -U fence_user index_db > index_dump.db
+docker exec -it $POSTGRES pg_dump -C -h localhost -U fence_user indexd_db > indexd_dump.db
 docker exec -it $POSTGRES pg_dump -C -h localhost -U peregrine_user metadata_db > metadata_dump.db
 docker exec -it $POSTGRES pg_dump -C -h localhost -U arborist_user arborist_db > arborist_dump.db
